9a478afe5c674ae882df4b51678c022ba51845ac
cup <mai.haoxiang@hzleaper.com>
2025-10-28 01:40:11 +0000

    1

--- src/SortedLinkedList.java
+++ src/SortedLinkedList.java
@@ -2,33 +2,34 @@ import java.util.ListIterator;
 import java.util.LinkedList;
 import java.lang.Comparable;
 
-public class SortedLinkedList<E extends Comparable<E>> extends LinkedList<E> {
+public class SortedLinkedList<E extends Comparable<? super E>> extends LinkedList<E> {
 
-    public boolean insert(E element) {
+    @Override
+    public boolean add(E e) {
         ListIterator<E> it = this.listIterator();//获取 有序链表的迭代器。这个迭代器的类型是:Iterator<E>
 //        if (this.size() == 0) {
-//            it.add(element);
+//            it.add(e);
 //            return true; //第一个元素
 //        }
         while (it.hasNext()) {
-            var next = it.next();
-            if (element.compareTo(next) < 0) {
+            E next = it.next();
+            if (e.compareTo(next) < 0) {
                 it.previous();
-                it.add(element);
+                it.add(e);
                 return true;
             }
         }
-        it.add(element);
+        it.add(e); //插入第一个元素，或插入到末尾
         return true;
     }
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SortedLinkedList<String> list = new SortedLinkedList<>();
-        list.insert("Bob");
-        list.insert("Sam");
-        list.insert("Amy");
+        list.add("Bob");
+        list.add("Sam");
+        list.add("Amy");
         for (String s : list) {
-            IO.println(s);
+            System.out.println(s);
         }
 
     }
